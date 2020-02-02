b5d2886126805cf8b516dcab94f16eea87b65460
Poulpy <paul.repain@yahoo.com>
2020-02-02 21:54:36 +0100

    J'ai oublié d'ajouté le répertoire des tests ^'

---
+++ test/ServerTest.java
@@ -0,0 +1,17 @@
+package test;
+
+//import junit.framework.*;
+import org.junit.*;
+/*
+import org.junit.Test;
+import org.junit.Assert.*;
+*/
+
+public class ServerTest {
+
+    @Test
+    public static void testUtilisateurExiste() throws Exception {
+        Assert.assertEquals(0, 0);
+    }
+}
+
---
+++ test/TestServer.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.*;
+
+public class TestServer {
+
+    @Test
+    public void testUtilisateurExiste() throws Exception {
+        Assert.assertEquals(0, 0);
+    }
+}
+
