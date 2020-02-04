638088b4c7fd8def9fc8eb564262e98112174bae
Poulpy <paul.repain@yahoo.com>
2020-02-04 12:34:21 +0100

    Joli bouton de recherche

--- client/Client.java
+++ client/Client.java
@@ -6,6 +6,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.control.Button;
 
 public class Client extends Application {
@@ -38,10 +40,30 @@ public class Client extends Application {
     public void start(Stage stage) {
         primaryStage = stage;
         Group root = new Group();
-        Scene scene = new Scene(root, 300, 300, Color.LIGHTBLUE);
+        Scene scene = new Scene(root, 500, 500, Color.WHITE);
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.setTitle("Hello World");
+
+        scene.getStylesheets().add("ss.css");
+        Button searchButton = new Button("Rechercher");
+        searchButton.setPrefWidth(100);
+        searchButton.setPrefHeight(40);
+        searchButton.getStyleClass().add("searchButton");
+        //searchButton.setArcWidth(30);
+        //searchButton.setArcHeight(30);
+        System.out.println(com.sun.javafx.runtime.VersionInfo.getRuntimeVersion());
+        /*
+        searchButton.setFill(
+            new LinearGradient(0f, 0f, 0f, 1f, true, CycleMethod.NO_CYCLE,
+                new Stop[] {
+                    new Stop(0, Color.web("#333333")),
+                    new Stop(1, Color.web("#000000"))
+                }
+            )
+        );
+        */
+        root.getChildren().add(searchButton);
     }
 }
 
