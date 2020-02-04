e2675f384d60ed70bc15877436d316811e1ec7ff
Poulpy <paul.repain@yahoo.com>
2020-02-04 14:18:38 +0100

    Ajouté une liste pour mettre les noms des fichiers :D !

--- client/Client.java
+++ client/Client.java
@@ -1,18 +1,21 @@
 package client;
 
 import java.io.IOException;
-import javafx.stage.Stage;
 import javafx.application.Application;
-import javafx.scene.Scene;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.KeyCode;
-import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import java.util.ArrayList;
 
 public class Client extends Application {
 
@@ -76,9 +79,31 @@ public class Client extends Application {
             }
         });
 
-        root.getChildren().add(searchField);
 
+        ListView<String> list = new ListView<String>();
+        ArrayList<String> filesMatching = samples();
+        for (int i = 0; i != filesMatching.size(); i++) {
+            list.getItems().add(filesMatching.get(i));
+        }
+        HBox hbox = new HBox(list);
+        hbox.setLayoutY(100);
+        hbox.setLayoutX(10);
+
+
+
+
+        root.getChildren().add(hbox);
+        root.getChildren().add(searchField);
         root.getChildren().add(searchButton);
+    }
+
+    public ArrayList<String> samples() {
+        ArrayList<String> a = new ArrayList<String>();
+        a.add("localhost/truc.txt");
+        a.add("poseidon/yojinbo");
+        a.add("zeus/RAPPORT.pdf");
+
+        return a;
     }
 }
 
