fdeba84dded437611f7a5db288b6e741bbd6a769
syrinebra <35915898+syrinebra@users.noreply.github.com>
2020-02-10 13:02:02 +0100

    Multithreading2

--- src/fr/uvsq/fsp/server/WaitingServer.java
+++ src/fr/uvsq/fsp/server/WaitingServer.java
@@ -1,17 +1,99 @@
 package fr.uvsq.fsp.server;
 
+import fr.uvsq.fsp.abstractions.Yoda;
+import java.util.ArrayList;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import fr.uvsq.fsp.util.FTPCommand;
+import java.nio.file.DirectoryStream;
 
-public class WaitingServer implements Runnable {
+public class WaitingServer extends Yoda implements Runnable{
+
+	
 	  Socket sock;
 	  Boolean isStopped;
 	  FSPCentral server1;
-	
+    /**
+     * Chemin du fichier contenant les utilisateurs connus du serveur
+     * l'identifiant et le hash du mot de passe sont séparés par le
+     * séparateur sep
+     */
+    public static String cheminUtilisateurs;
+
+    private String sep = ",";
+
+    /** Répertoire des fichiers partagés, créé au lancement du serveur */
+    public static String descriptionsFolder;
+
+    public ServerSocket serverSocket;
+
+    /** Liste des utilisateurs connectés */
+    public ArrayList<String> usersConnected;
+
+    // Attributs propre à un client :
+    // @Thread
+    private String id;
+
+    private String mdp;
+
+    private boolean nouvelUtilisateur = false;
+
+    public String hostname = "dinfo";
+
+    public String userDescriptionFolder;
+
+ 
+    public WaitingServer(String serverIP, int port, Socket sock) {
+    	
+    	super(serverIP, port);
+    	
+    	socket =sock;
+    	
+    }
+
+    public void disconnect() {
+        try {
+           
+            serverSocket.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void connect() throws IOException, UnknownHostException {
+        serverSocket = new ServerSocket(port, 10);
+        
+    }
 	public synchronized void stop() {
 		
 	isStopped=true;
-	server1.disconnect();
+    disconnect();
 	
 	}
 	
@@ -27,7 +109,7 @@ public class WaitingServer implements Runnable {
 		while(!isStopped) {
  
 			try {
-				sock = server1.serverSocket.accept();
+				sock = serverSocket.accept();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -38,7 +120,9 @@ public class WaitingServer implements Runnable {
 		}
 		
 	}
-	
-	
-
+    
+    
+    
 }
+
+
