0c7feed071611844c7c2286ef18171254ba4734e
Poulpy <paul.repain@yahoo.com>
2020-02-20 09:57:51 +0100

    Ajout bouton pour rafraichir la connexion <3

--- src/fr/uvsq/fsp/view/ClientView.java
+++ src/fr/uvsq/fsp/view/ClientView.java
@@ -1,6 +1,8 @@
 package fr.uvsq.fsp.view;
 
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
@@ -8,6 +10,8 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -26,130 +30,145 @@ import javafx.util.Duration;
 
 public class ClientView extends Group {
 
-    /**
-     * Button used to query the server.
-     * A keyword is sent, and the server tries to find files that matches the keyword.
-     */
-    public Button downloadButton;
+	/**
+	 * Button used to query the server.
+	 * A keyword is sent, and the server tries to find files that matches the keyword.
+	 */
+	public Button downloadButton;
 
-    /**
-     * Button to query the server.
-     */
-    public Button searchButton;
+	/**
+	 * Button to query the server.
+	 */
+	public Button searchButton;
+	public Button refreshConnectionButton;
 
-    /**
-     * List showing the results/files sent by the server.
-     */
-    public ListView<String> fileList;
+	/**
+	 * List showing the results/files sent by the server.
+	 */
+	public ListView<String> fileList;
 
-    /**
-     * Receives the keyword to be sent to the server.
-     */
-    public TextField searchField;
-    public TextField serverIPField;
-    public TextField portField;
+	/**
+	 * Receives the keyword to be sent to the server.
+	 */
+	public TextField searchField;
+	public TextField serverIPField;
+	public TextField portField;
 
-    /**
-     * Pops when a user downloaded successfully.
-     */
-    public Label downloadLabel;
+	/**
+	 * Pops when a user downloaded successfully.
+	 */
+	public Label downloadLabel;
 
-    public ClientView() {
-        GridPane grid = new GridPane();
+	public ClientView() {
+		GridPane grid = new GridPane();
 
-        getStylesheets().add("client/stylesheet.css");
+		getStylesheets().add("client/stylesheet.css");
 
-        serverIPField = new TextField();
-        serverIPField.getStyleClass().add("removeLightGlow");
-        serverIPField.getStyleClass().add("searchField");
-        serverIPField.setPrefWidth(70);
-        serverIPField.setPrefHeight(10);
+		serverIPField = new TextField();
+		serverIPField.getStyleClass().add("removeLightGlow");
+		serverIPField.getStyleClass().add("searchField");
+		serverIPField.setPrefWidth(70);
+		serverIPField.setPrefHeight(10);
 
-        portField = new TextField();
-        portField.getStyleClass().add("removeLightGlow");
-        portField.getStyleClass().add("searchField");
-        portField.setPrefWidth(50);
-        portField.setPrefHeight(10);
+		portField = new TextField();
+		portField.getStyleClass().add("removeLightGlow");
+		portField.getStyleClass().add("searchField");
+		portField.setPrefWidth(50);
+		portField.setPrefHeight(10);
 
-        // Bouton de recherche
-        searchButton = new Button("Rechercher");
-        searchButton.setPrefWidth(120);
-        searchButton.setPrefHeight(35);
-        searchButton.getStyleClass().add("searchButton");
-        searchButton.getStyleClass().add("removeLightGlow");
-        searchButton.setLayoutX(30);
-        searchButton.setLayoutY(50);
+		Image refreshConnectionImage;
+		refreshConnectionButton = new Button();
+		try {
+			refreshConnectionImage = new Image(new FileInputStream("client/refresh.png"));
+			refreshConnectionButton.setGraphic(new ImageView(refreshConnectionImage));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		refreshConnectionButton.getStyleClass().add("searchButton");
+		refreshConnectionButton.getStyleClass().add("removeLightGlow");
+		refreshConnectionButton.setPrefWidth(10);
+		refreshConnectionButton.setPrefHeight(10);
+
+		// Bouton de recherche
+		searchButton = new Button("Rechercher");
+		searchButton.setPrefWidth(120);
+		searchButton.setPrefHeight(35);
+		searchButton.getStyleClass().add("searchButton");
+		searchButton.getStyleClass().add("removeLightGlow");
+		searchButton.setLayoutX(30);
+		searchButton.setLayoutY(50);
 
 
-        // Champ de recherche
-        searchField = new TextField();
-        searchField.getStyleClass().add("removeLightGlow");
-        searchField.getStyleClass().add("searchField");
-        searchField.setPrefWidth(150);
-        searchField.setPrefHeight(35);
-        searchField.setLayoutX(30);
-        searchField.setLayoutY(10);
+		// Champ de recherche
+		searchField = new TextField();
+		searchField.getStyleClass().add("removeLightGlow");
+		searchField.getStyleClass().add("searchField");
+		searchField.setPrefWidth(150);
+		searchField.setPrefHeight(35);
+		searchField.setLayoutX(30);
+		searchField.setLayoutY(10);
 
 
 
-        // Liste affichant les fichiers qui correspondent à la recherche
-        fileList = new ListView<String>();
-        fileList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        fileList.setPrefHeight(200);
-        fileList.setPrefWidth(150);
-        fileList.setOrientation(Orientation.VERTICAL);
+		// Liste affichant les fichiers qui correspondent à la recherche
+		fileList = new ListView<String>();
+		fileList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		fileList.setPrefHeight(200);
+		fileList.setPrefWidth(150);
+		fileList.setOrientation(Orientation.VERTICAL);
 
-        // Bouton de téléchargement des fichiers
-        downloadButton = new Button("Télécharger");
-        downloadButton.setPrefWidth(120);
-        downloadButton.setPrefHeight(35);
-        downloadButton.getStyleClass().add("searchButton");
-        downloadButton.getStyleClass().add("removeLightGlow");
+		// Bouton de téléchargement des fichiers
+		downloadButton = new Button("Télécharger");
+		downloadButton.setPrefWidth(120);
+		downloadButton.setPrefHeight(35);
+		downloadButton.getStyleClass().add("searchButton");
+		downloadButton.getStyleClass().add("removeLightGlow");
 
-        downloadLabel = new Label();
-        downloadLabel.getStyleClass().add("greenFont");
+		downloadLabel = new Label();
+		downloadLabel.getStyleClass().add("greenFont");
 
-        grid.add(serverIPField, 0, 0, 1, 1);
-        grid.add(portField, 1, 0, 1, 1);
+		grid.add(serverIPField, 0, 0, 1, 1);
+		grid.add(portField, 1, 0, 1, 1);
+		grid.add(refreshConnectionButton, 2, 0, 1, 1);
 
-        grid.add(searchField, 0, 1, 2, 1);
-        grid.add(searchButton, 2, 1, 1, 1);
+		grid.add(searchField, 0, 1, 2, 1);
+		grid.add(searchButton, 2, 1, 1, 1);
 
-        grid.add(fileList, 0, 2, 2, 2);
-        grid.add(downloadButton, 2, 2, 1, 1);
-        grid.add(downloadLabel, 2, 3, 1, 1);
+		grid.add(fileList, 0, 2, 2, 2);
+		grid.add(downloadButton, 2, 2, 1, 1);
+		grid.add(downloadLabel, 2, 3, 1, 1);
 
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setLayoutX(10);
-        grid.setLayoutY(10);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setLayoutX(10);
+		grid.setLayoutY(10);
 
-        // On affiche les éléments dans la fenêtre
-        this.getChildren().add(grid);
-    }
+		// On affiche les éléments dans la fenêtre
+		this.getChildren().add(grid);
+	}
 
-    /**
-     * Met à jour la liste de fichiers dans la vue
-     * Supprime les éléments qui étaient déjà dans la liste
-     */
-    public void updateListView(ArrayList<String> filesMatching) {
-        fileList.getItems().clear();
+	/**
+	 * Met à jour la liste de fichiers dans la vue
+	 * Supprime les éléments qui étaient déjà dans la liste
+	 */
+	public void updateListView(ArrayList<String> filesMatching) {
+		fileList.getItems().clear();
 
-        for (int i = 0; i != filesMatching.size(); i++) {
-            fileList.getItems().add(filesMatching.get(i));
-        }
-    }
+		for (int i = 0; i != filesMatching.size(); i++) {
+			fileList.getItems().add(filesMatching.get(i));
+		}
+	}
 
-    /**
-     * Animation sur un label : fait apparaître puis disparaître le text
-     */
-    public void fadeAnimation(Label text) {
-        FadeTransition ft = new FadeTransition(Duration.millis(2000), text);
-        ft.setFromValue(0.0);
-        ft.setToValue(1.0);
-        ft.setCycleCount(2);
-        ft.setAutoReverse(true);
-        ft.playFromStart();
-    }
+	/**
+	 * Animation sur un label : fait apparaître puis disparaître le text
+	 */
+	public void fadeAnimation(Label text) {
+		FadeTransition ft = new FadeTransition(Duration.millis(2000), text);
+		ft.setFromValue(0.0);
+		ft.setToValue(1.0);
+		ft.setCycleCount(2);
+		ft.setAutoReverse(true);
+		ft.playFromStart();
+	}
 }
 
