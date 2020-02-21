d4c9fe93902ecbc8eb006eaed4593c1bdb445d4c
Poulpy <paul.repain@yahoo.com>
2020-02-21 15:39:09 +0100

    Corrections des tests + doc

--- src/fr/uvsq/fsp/util/PatternFinder.java
+++ src/fr/uvsq/fsp/util/PatternFinder.java
@@ -14,6 +14,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
+/**
+ * Similaire à grep
+ */
 public class PatternFinder {
 
     /**
--- src/test/TestServer.java
+++ src/test/TestServer.java
@@ -23,63 +23,10 @@ public class TestServer {
 		central = new FSPCentral(sock, "build/test/central/");
 	}
 
-
 	@Test
-	public void testSearchByKeyword() throws Exception {
-		//central.handleHostnameCommand("dinfo");
-		File fileToSearch = new File(central.descriptionsFolder + "dinfo/yojinbo");
-		Assert.assertTrue(central.searchByKeyword("Le", fileToSearch));
-		Assert.assertTrue(central.searchByKeyword("garde", fileToSearch));
-		Assert.assertTrue(central.searchByKeyword("du", fileToSearch));
-		Assert.assertFalse(central.searchByKeyword("Truc", fileToSearch));
-	}
-
-	@Test
-	public void testSearchFolderByKeyword() throws Exception {
-		String folderToSearch = central.descriptionsFolder + "dinfo/";
-		ArrayList<String> emp = new ArrayList<String>();
-		Map<String, Boolean> keywords = new HashMap<String, Boolean>();
-		ArrayList<String> matches = new ArrayList<String>();
-
-		keywords.put("Le", true);
-		keywords.put("garde", true);
-		keywords.put("du", true);
-		keywords.put("film", true);
-		keywords.put("Truc", false);
-
-		for (Map.Entry<String, Boolean> keyword : keywords.entrySet()) {
-
-			matches = central.searchFolderByKeyword(keyword.getKey(), folderToSearch);
-			/*
-			if (matches.size() > 0)
-				System.out.println(matches.get(0).toString());
-				*/
-
-			if (keyword.getValue()) {
-				//System.out.println(matches);
-				Assert.assertTrue(matches.size() > 0);
-			} else {
-				Assert.assertEquals(matches.size(), 0);
-			}
-		}
-	}
-
-	@Test
-	public void testSearchUsersFoldersByKeyword() throws Exception {
-		ArrayList<String> connected = new ArrayList<String>();
-		ArrayList<String> files = new ArrayList<String>();
-
-		files = central.searchUsersFoldersByKeyword("Le");
-		// Pas d'utilisateurs : size() == 0
-		Assert.assertEquals(files.size(), 0);
-		central.usersConnected.add("dinfo");
-		// size() == 1
-		files = central.searchUsersFoldersByKeyword("étoiles");
-		Assert.assertEquals(files.size(), 1);
-
-		files = central.searchUsersFoldersByKeyword("film");
-		//System.out.println(files);
-		Assert.assertEquals(files.size(), 2);
+	public void testIsClientLogged() throws Exception {
+		central.hostname = "dinfo";
+		Assert.assertFalse(central.isClientLogged());
 	}
 }
 
