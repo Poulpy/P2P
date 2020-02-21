2fbb3200c0364095a486115fa27d4a7318133161
Poulpy <paul.repain@yahoo.com>
2020-02-21 11:17:40 +0100

    CSVParser write() works fine !

--- src/fr/uvsq/fsp/util/CSVParser.java
+++ src/fr/uvsq/fsp/util/CSVParser.java
@@ -12,31 +12,39 @@ import java.io.FileWriter;
 
 public class CSVParser {
 
-	public static Map<String, String> read(String filePath) {
-		Map<String, String> map;
-		String values[];
-		String line;
-		BufferedReader reader;
+    public static Map<String, String> read(String filePath) throws IOException {
+        Map<String, String> map;
+        String values[];
+        String line;
+        BufferedReader reader;
 
-		map = new HashMap<String, String>();
-		values = new String[2];
+        map = new HashMap<String, String>();
+        values = new String[2];
 
-		try {
-			reader = new BufferedReader(new FileReader(filePath));
+        reader = new BufferedReader(new FileReader(filePath));
 
-			// Lecture de chaque ligne
-			while ((line = reader.readLine()) != null) {
-				values = line.split(",");
+        // Lecture de chaque ligne
+        while ((line = reader.readLine()) != null) {
+            values = line.split(",");
 
-				map.put(values[0], values[1]);
-			}
+            map.put(values[0], values[1]);
+        }
 
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        reader.close();
 
-		return map;
-	}
+        return map;
+    }
+
+    public static void write(Map<String, String> map, String filePath) throws IOException {
+        BufferedWriter writer;
+
+        writer = new BufferedWriter(new FileWriter(filePath));
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            writer.write(entry.getKey() + "," + entry.getValue() + System.getProperty("line.separator"));
+        }
+
+        writer.close();
+    }
 }
 
--- src/test/TestCSVParser.java
+++ src/test/TestCSVParser.java
@@ -11,13 +11,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TestCSVParser {
+
     @Test
     public void testRead() throws Exception {
         Map<String, String> users;
 
         users = new HashMap<String, String>();
 
-        users = CSVParser.read("build/test/central/utilisateurs.csv");
+        try {
+            users = CSVParser.read("build/test/central/utilisateurs.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         for (Map.Entry<String, String> entry : users.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
@@ -27,6 +32,31 @@ public class TestCSVParser {
         Assert.assertTrue(users.containsKey("toto"));
         Assert.assertEquals(users.get("toto"), "21232f297a57a5a743894a0e4a801fc3");
         Assert.assertEquals(users.get("zoiehoziroa"), null);
+    }
+
+    @Test
+    public void testWrite() throws Exception {
+        Map<String, String> users;
+
+        users = new HashMap<String, String>();
+
+        try {
+            users = CSVParser.read("build/test/central/utilisateurs.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        users.put("Sayous", "OwO");
+        CSVParser.write(users, "build/test/central/test.csv");
+
+        try {
+            users = CSVParser.read("build/test/central/test.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(users.size(), 6);
+        Assert.assertTrue(users.containsKey("Sayous"));
+        Assert.assertEquals(users.get("Sayous"), "OwO");
     }
 }
 
